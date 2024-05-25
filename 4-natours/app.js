0c51716215e350a912bf08ba7eef555c29008650
Evan <eroberts1998@icloud.com>
2024-05-24 20:49:00 -0400

    section 6 progress

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const fs = require('fs')
+const morgan = require('morgan')
 
 const app = express()
-
+app.use(morgan('dev'))
 app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log('hello this is middleware')
+    next()
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    next()
+})
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -15,9 +26,25 @@ app.get('/api/v1/tours', (req, res) => {
             tours: tours
         }
     })
-})
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+    console.log(req.params)
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+
+    if (!tour) return res.status(404).json({status:'failed', message:'Invalid id'})
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tour
+        }
+    })
+}
+
+const createTour = (req, res) => {
     // console.log(req.body)
     const newId = tours[tours.length - 1].id + 1
     const newTour = Object.assign({id: newId}, req.body)
@@ -30,8 +57,40 @@ app.post('/api/v1/tours', (req, res) => {
             }
         })
     })
-})
+}
 
+const updateTour = (req, res) => {
+
+    if (req.params.id * 1 > tours.length) return res.status(404).json({status:'failed', message:'Invalid id'})
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here>'
+        }
+    })
+}
+
+const deleteTour = (req, res) => {
+
+    if (req.params.id * 1 > tours.length) return res.status(404).json({status:'failed', message:'Invalid id'})
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+}
+
+const getAllUsers = (req, res) => { return res.status(500).json({status:'error', message:'This route is not yet defined'})}
+const getUser = (req, res) => {return res.status(500).json({status:'error', message:'This route is not yet defined'})}
+const createUser = (req, res) => {return res.status(500).json({status:'error', message:'This route is not yet defined'})}
+const updateUser = (req, res) => {return res.status(500).json({status:'error', message:'This route is not yet defined'})}
+const deleteUser = (req, res) => {return res.status(500).json({status:'error', message:'This route is not yet defined'})}
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
+app.route('/api/v1/users').get(getAllUsers).post(createUser)
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser)
 const port = 3000
 app.listen(port, () => {
     console.log(`running on port ${port}`)
