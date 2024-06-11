e765d9d1a95d71670b31e078571006aa1bd8ffae
Evan <eroberts1998@icloud.com>
2024-06-11 11:14:28 -0400

    section 8 progrss

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -14,7 +14,35 @@ exports.getAllTours = async (req, res) => {
       /\b(gte|gt|lte|lt)\b/g,
       (match) => `$${match}`
     );
-    const query = Tour.find(JSON.parse(queryString));
+
+    let query = Tour.find(JSON.parse(queryString));
+
+    // Sorting
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt')
+    }
+
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v')
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit)
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist')
+    }
 
     // Query Request with advanced filtering
     // 127.0.0.1:8000/api/v1/tours?difficulty=easy&duration[gte]=5
